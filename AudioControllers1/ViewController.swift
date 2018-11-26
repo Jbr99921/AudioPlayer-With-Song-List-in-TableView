c721069c7fc71bc13d36e2652dfbb396a4427864
JigneshRajora <jigneshrajora@jigneshrajoras-mac.local>
2018-11-25 19:20:03 -0800

    Initial Commit

---
+++ AudioControllers1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioControllers1
+//
+//  Created by JigneshRajora on 25/11/18.
+//  Copyright © 2018 JigneshRajora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AudioControllers1Tests/AudioControllers1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AudioControllers1Tests.swift
+//  AudioControllers1Tests
+//
+//  Created by JigneshRajora on 25/11/18.
+//  Copyright © 2018 JigneshRajora. All rights reserved.
+//
+
+import XCTest
+@testable import AudioControllers1
+
+class AudioControllers1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
